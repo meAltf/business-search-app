c33b0d823ad321ccc953551c7011684b8e227169
meAltf <alataf.dce.cs1@gmail.com>
2026-02-11 00:34:56 +0530

    add exception

--- src/main/java/com/alataf/business/search/dto/SuggestionRequestParameters.java
+++ src/main/java/com/alataf/business/search/dto/SuggestionRequestParameters.java
@@ -1,5 +1,21 @@
 package com.alataf.business.search.dto;
 
+import com.alataf.business.search.exceptions.BadRequestException;
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
+
 public record SuggestionRequestParameters(String prefix,
                                           Integer limit) {
+
+    public SuggestionRequestParameters{
+        if(!StringUtils.hasText(prefix)){
+           // try {
+                throw new BadRequestException("Prefix can not empty");
+           // } catch (BadRequestException e) {
+            //    throw new RuntimeException(e);
+           // }
+        }
+        limit = Objects.requireNonNullElse(limit, 10);
+    }
 }
---
+++ src/main/java/com/alataf/business/search/exceptions/BadRequestException.java
@@ -0,0 +1,9 @@
+package com.alataf.business.search.exceptions;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+}
